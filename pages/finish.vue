a725a6acfb99a3ae372214f12a3ba0a3d983056a
Rasmus Schlunsen <rasmus@schlunsen.com>
2023-01-09 22:42:44 +0100

    minor updates

---
+++ pages/finish.vue
@@ -0,0 +1,212 @@
+<script lang="ts" setup>
+// composable
+const { t } = useLang()
+
+// meta
+definePageMeta({
+  layout: 'page',
+})
+
+// vars
+const titlesText = computed<string[]>(() => t('pages.index.title').split('[]'))
+const leadingsText = computed(() => [
+  {
+    text: titlesText.value[0],
+    startColor: '#007CF0',
+    endColor: '#00DFD8',
+    delay: 0,
+  },
+  {
+    text: titlesText.value[1],
+    startColor: '#7928CA',
+    endColor: '#FF0080',
+    delay: 2,
+  },
+  {
+    text: titlesText.value[2],
+    startColor: '#FF4D4D',
+    endColor: '#F9CB28',
+    delay: 4,
+  },
+])
+const tooltip = ref(false)
+
+// const
+const cancelTooltip = () => {
+  tooltip.value = false
+  const tt = document.querySelector('.tooltiptext')
+  if (tt) tt.innerHTML = `Copy to clipboard`
+}
+const copyBash = () => {
+  const bash = 'git clone https://github.com/viandwi24/nuxt3-awesome-starter'
+  navigator.clipboard.writeText(bash)
+  tooltip.value = true
+  const tt = document.querySelector('.tooltiptext')
+  if (tt) tt.innerHTML = `Copied!!!`
+}
+</script>
+
+<template>
+  <PageWrapper class="flex-1 flex">
+    <PageBody class="flex-1 flex">
+      <PageSection >
+        <div class="max-w-8xl w-full mx-auto" >
+          <div class="w-full mb-8 max-w-8xl w-full mx-auto">
+            <h2 class="text-3xl font-bold mb-2">
+              Thank you for registering - You are now verified!
+            </h2>
+            
+          </div>
+        </div>
+      </PageSection>
+    </PageBody>
+  </PageWrapper>
+</template>
+
+<style lang="scss">
+@import '../assets/sass/variables';
+
+@keyframes anim-fg-1 {
+  0%,
+  16.667%,
+  100% {
+    opacity: 1;
+  }
+
+  33.333%,
+  83.333% {
+    opacity: 0;
+  }
+}
+
+@keyframes anim-fg-2 {
+  0%,
+  16.667%,
+  66.667%,
+  100% {
+    opacity: 0;
+  }
+
+  33.333%,
+  50% {
+    opacity: 1;
+  }
+}
+
+@keyframes anim-fg-3 {
+  0%,
+  50%,
+  100% {
+    opacity: 0;
+  }
+
+  66.667%,
+  83.333% {
+    opacity: 1;
+  }
+}
+
+.animated-text-bg {
+  position: relative;
+  display: block;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  content: var(--content);
+  display: block;
+  width: 100%;
+  color: theme('colors.slate.800');
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 0;
+  padding-left: $padding;
+  padding-right: $padding;
+  &:before {
+    content: var(--content);
+    position: absolute;
+    display: block;
+    width: 100%;
+    color: theme('colors.slate.800');
+    top: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 0;
+    padding-left: $padding;
+    padding-right: $padding;
+  }
+}
+.animated-text-fg {
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  padding-left: $padding;
+  padding-right: $padding;
+  background-image: linear-gradient(
+    90deg,
+    var(--start-color),
+    var(--end-color)
+  );
+  position: relative;
+  opacity: 0;
+  z-index: 1;
+  animation: var(--animation-name) 8s infinite;
+}
+
+html.dark {
+  .animated-text-bg {
+    color: theme('colors.gray.100');
+    &:before {
+      color: theme('colors.gray.100');
+    }
+  }
+}
+
+.triangle-shape {
+  width: 0;
+  height: 0;
+  border-left: 25px solid transparent;
+  border-right: 25px solid transparent;
+  border-bottom: 40px solid theme('colors.green.600');
+  transform: translate(-15rem, -6rem) rotate(45deg);
+}
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 140px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  bottom: 150%;
+  left: 50%;
+  margin-left: -75px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.tooltip .tooltiptext::after {
+  content: '';
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
